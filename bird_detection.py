0f8b4bc6e79437a9cc21616bf18bb6c5ea6cfe30
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-03-18 17:09:23 -0600

    add implementation to play hawk screech upon bird detection

--- bird_detection.py
+++ bird_detection.py
@@ -1,42 +1,71 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
+import playsound as ps
+from threading import Thread
+
+REQUIRED_DETECTIONS = 10
 
 
-def detectAndDisplay(frame):
+def detect_and_display(frame, bird_cascade):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
-    # -- Detect faces
     birds = bird_cascade.detectMultiScale(frame_gray)
     for (x, y, w, h) in birds:
         center = (x + w // 2, y + h // 2)
         frame = cv.ellipse(frame, center, (w // 2, h // 2), 0, 0, 360, (255, 0, 255), 4)
-        # -- In each face, detect eyes
+
+    font = cv.FONT_HERSHEY_SIMPLEX
+    org = (50, 50)
+    font_scale = 0.6
+    color = (255, 0, 0)
+    thickness = 1
+    detected = len(birds) > 0
+    if detected:
+        frame = cv.putText(frame, 'Detected Bird!', org, font, font_scale, color, thickness, cv.LINE_AA)
     cv.imshow('Capture - Bird detection', frame)
+    return detected
 
 
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--bird_cascade', help='Path to face cascade.',
-                    default='data/haarcascades/haarcascade_bird.xml')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-args = parser.parse_args()
-bird_cascade_name = args.bird_cascade
-bird_cascade = cv.CascadeClassifier()
-# -- 1. Load the cascades
-if not bird_cascade.load(cv.samples.findFile(bird_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-camera_device = args.camera
-# -- 2. Read the video stream
-cap = cv.VideoCapture(camera_device)
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
-while True:
-    ret, frame = cap.read()
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
-        break
-    detectAndDisplay(frame)
-    if cv.waitKey(10) == 27:
-        break
+def main():
+    # get arguments
+    parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
+    parser.add_argument('--bird_cascade', help='Path to face cascade.',
+                        default='data/haarcascades/haarcascade_bird.xml')
+    parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+    args = parser.parse_args()
+    bird_cascade_name = args.bird_cascade
+    bird_cascade = cv.CascadeClassifier()
+
+    # Load the cascades
+    if not bird_cascade.load(cv.samples.findFile(bird_cascade_name)):
+        print('--(!)Error loading face cascade')
+        exit(0)
+    camera_device = args.camera
+    # Read the video stream
+    cap = cv.VideoCapture(camera_device)
+    if not cap.isOpened:
+        print('--(!)Error opening video capture')
+        exit(0)
+
+    num_consecutive_detections = 0
+    # Detection loop
+    while True:
+        ret, frame = cap.read()
+        if frame is None:
+            print('--(!) No captured frame -- Break!')
+            break
+        detected = detect_and_display(frame, bird_cascade)
+        if detected:
+            num_consecutive_detections += 1
+            if num_consecutive_detections >= REQUIRED_DETECTIONS:
+                Thread(target=ps.playsound, args=('data/sounds/hawk-scream.mp3',)).start()
+                num_consecutive_detections = 0
+        else:
+            num_consecutive_detections = 0
+        if cv.waitKey(10) == 27:
+            break
+
+
+if __name__ == '__main__':
+    main()
